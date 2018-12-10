7bdffc9a39bf78d936c81ba881d63351c0136524
Abhishek Kumar <abhishekisme1996@hotmail.com>
2018-12-10 14:47:34 +0530

    Maximum Sub Array Sum Solution

--- Max_SumArray_Sum/main.cpp
+++ Max_SumArray_Sum/main.cpp
@@ -4,6 +4,30 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int t;
+    cin>>t;
+    int result[t];
+    int k=0;
+    while(t>0)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        int cs=0,ms=0;
+        for(int i=0;i<n;i++)
+        {
+            cs+=a[i];
+            if(cs<0)
+                cs=0;
+            ms=max(cs,ms);
+        }
+        result[k]=ms;
+        k++;
+        t--;
+    }
+    for(int i=0;i<=k;i++)
+       cout<<result[i]<<endl;
     return 0;
 }
