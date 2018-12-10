933f86d56e50673b513c0cd6c2ab58a2607fea5a
Abhishek Kumar <abhishekisme1996@hotmail.com>
2018-12-10 13:45:53 +0530

    Rain Water Harvesting Problem complete solution

--- RainWaterHarvesting/main.cpp
+++ RainWaterHarvesting/main.cpp
@@ -4,6 +4,48 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int N;
+    cin>>N;
+    int A[N],left[N],right[N];
+    for(int i=0;i<N;i++)
+        cin>>A[i];
+        int left_max_A=A[0];
+        left[0]=A[0];
+        int right_max_A=A[N-1];
+        right[N-1]=right_max_A;
+        int sum=0;
+    for(int i=1;i<N;i++)
+    {
+        if(A[i]<left_max_A)
+        {
+            left[i]=left_max_A;
+        }
+        else
+        {
+            left[i]=A[i];
+            left_max_A=A[i];
+        }
+    }
+    for(int i=N-2;i>=0;i--)
+    {
+        if(A[i]<right_max_A)
+            right[i]=right_max_A;
+        else
+        {
+            right[i]=A[i];
+            right_max_A=A[i];
+        }
+    }
+    for(int i=0;i<N;i++)
+    {
+        sum+=(min(left[i],right[i])-A[i]);
+    }
+    for(int i=0;i<N;i++)
+        cout<<left[i]<<" ";
+        cout<<endl;
+         for(int i=0;i<N;i++)
+        cout<<right[i]<<" ";
+        cout<<endl;
+        cout<<sum;
     return 0;
 }
